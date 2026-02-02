a02511f605b44f3bf0155c714f01a54d356cc1fe
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-02 16:49:50 +0600

    Math, Abs, Round, Ceil, Floor, And Random number

---
+++ math.js
@@ -0,0 +1,26 @@
+// const Math = {
+//     min: function min(num1, num2){
+//         return num2;
+
+//     }
+// }
+
+// console.log(Math.max);
+
+const min = Math.min(45,1,6,99,103,120);
+console.log(min)
+
+const max = Math.max(45,1,6,99,103,120);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-10));
+console.log(Math.round(2.25))
+console.log(Math.round(2.85))
+console.log(Math.floor(2.95));
+console.log(Math.ceil(2.1))
+
+console.log(Math.random()*10)
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
